26b44c6987699922c9b439b3c0569439033997cc
Pranay00 <pranaywawalkar2005.com>
2026-03-29 07:16:38 -0700

    Init

--- Q1_Q2/db.php
+++ Q1_Q2/db.php
@@ -2,7 +2,7 @@
 $servername='localhost';
 $usernamae='root';
 $password='';
-$dbname='employee_db';
+$dbname='j2_php';
 $conn=new mysqli($servername,$usernamae,$password,$dbname);
 if (!$conn) {
     echo "Db not connected";
